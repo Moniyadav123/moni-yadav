4c6f1073db425681b1ae9e5688813597f8675ea1
Moni yadav <117387766+Moniyadav123@users.noreply.github.com>
2023-03-15 15:16:15 +0530

    Add files via upload

---
+++ runningsumofarray.java
@@ -0,0 +1,12 @@
+public class runningsumofarray {
+    public static void main(String[] args) {
+        int a[]={1,3,3,4,5,6,7,0,7,1,6};
+        int sum=0;
+        for(int i=0;i<a.length;i++){
+            sum=sum+a[i];
+            System.out.println(sum);
+
+        }
+    }
+    
+}
